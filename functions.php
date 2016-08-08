3e5049f5d133bcfa1727c2d1bb1f1a9dfc6daf24
bukssaayman <bukssaayman@hotmail.com>
2016-08-08 14:37:54 +0200

    window.opener.location exploit fix
    fix the window.opener.location vulnerability

---
+++ functions.php
@@ -0,0 +1,28 @@
+function my_content_filter($html) {
+    $dom = new DOMDocument();
+    libxml_use_internal_errors(true);
+    $dom->loadHTML($html);
+
+    //Evaluate Anchor tag in HTML
+    $xpath = new DOMXPath($dom);
+    $hrefs = $xpath->evaluate("/html/body//a");
+
+    if ($hrefs->length > 0) {
+        for ($i = 0; $i < $hrefs->length; $i++) {
+            $href = $hrefs->item($i);
+            if ($href->getAttribute("target") == "_blank") {
+                $old_rel = $href->getAttribute("rel");
+                $new_rel = "noreferrer";
+                if (!empty($old_rel)) {
+                    $new_rel = $new_rel . " " . $old_rel;
+                }
+                $href->setAttribute("rel", $new_rel);
+            }
+        }
+        $html = $dom->saveHTML();
+    }
+
+    return $html;
+}
+
+add_filter("the_content", "my_content_filter");
