2e40ea9f8c031a899adbe7d6f1a18fe857e6485b
bukssaayman <bukssaayman@hotmail.com>
2016-08-24 11:21:18 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,28 +1,40 @@
 function my_content_filter($html) {
-    $dom = new DOMDocument();
-    libxml_use_internal_errors(true);
-    $dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
+   if (empty($html)) {
+		return $html;
+	}
 
-    //Evaluate Anchor tag in HTML
-    $xpath = new DOMXPath($dom);
-    $hrefs = $xpath->evaluate("/html/body//a");
+	$dom = new DOMDocument();
+	libxml_use_internal_errors(true);
+	$dom->loadXML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8')); // http://stackoverflow.com/questions/4879946/how-to-savehtml-of-domdocument-without-html-wrapper#answer-4880227
 
-    if ($hrefs->length > 0) {
-        for ($i = 0; $i < $hrefs->length; $i++) {
-            $href = $hrefs->item($i);
-            if ($href->getAttribute("target") == "_blank") {
-                $old_rel = $href->getAttribute("rel");
-                $new_rel = "noreferrer";
-                if (!empty($old_rel)) {
-                    $new_rel = $new_rel . " " . $old_rel;
-                }
-                $href->setAttribute("rel", $new_rel);
-            }
-        }
-        $html = $dom->saveHTML();
-    }
+	//Evaluate Anchor tag in HTML
+	$xpath = new DOMXPath($dom);
+	$hrefs = $xpath->evaluate("//a");
 
-    return $html;
+	if ($hrefs->length > 0) {
+		for ($i = 0; $i < $hrefs->length; $i++) {
+			$href = $hrefs->item($i);
+
+			if ($href->getAttribute('target') == '_blank') {
+				$old_rel = $href->getAttribute('rel');
+
+				if (empty($old_rel)) {
+					$new_rel = 'noreferrer';
+				}
+				else {
+					if (strpos('noreferrer', $old_rel) === false) {
+						$new_rel = 'noreferrer ' . $old_rel;
+					}
+				}
+
+				$href->setAttribute('rel', $new_rel);
+			}
+		}
+
+		$html = $dom->saveHTML();
+	}
+
+	return $html;
 }
 
 add_filter("the_content", "my_content_filter");
